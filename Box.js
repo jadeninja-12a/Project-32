b47fe9fd1cfcf32fd4c1848e15b8c6fec210c9cb
rdyava <47518433+rdyava@users.noreply.github.com>
2020-07-04 13:59:23 +0530

    commit

--- Box.js
+++ Box.js
@@ -1,16 +1,52 @@
-class Box{
-  constructor(x,y,width,height) {
-    this.body = Bodies.rectangle(x,y,width,height);
-    this.width = width;
-    this.color
-    this.height = height;
+class Box {
+  constructor(x, y,width,height ){
+    var options = {}
+  this.body = Bodies.rectangle(x, y, width, height, options);
+  this.width = width;
+  this.height = height;
+    this.image = loadImage("wood1.png");
+    this.visibility = 255;
+    this.flag = true;
     World.add(world, this.body);
+    
   }
-  display(){
-    var pos =this.body.position;
-    rectMode(CENTER);
-    fill(this.color);
-    rect(pos.x, pos.y, this.width, this.height);
+
+ display(){
+  if(this.body.speed>3 ){
+    this.flag = false;
+   
+  } else{
+    if(this.flag){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
   }
+  if(!this.flag){
+    push();
+    
+    if(this.visibility < 0){
+      World.remove(world, this.body);
+    }
+    this.visibility -= 5;
+    tint(255, this.visibility);
+    image(this.image, this.body.position.x,this.body.position.y, this.width, this.height);
+    pop();
+   }
+   
+   
+ }
+  score(){
+    if(this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+  }
+
+
+
 
 };
